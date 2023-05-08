a5a8c21a5398df32af5d741d667e2bd07f0b9532
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-05-08 14:13:53 -0600

    final touches

--- Main/client/src/js/database.js
+++ Main/client/src/js/database.js
@@ -12,10 +12,10 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Method that accepts some content and adds it to the database
 export const putDb = async (content) => {console.error('putDb not implemented');
 
-const contactDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1);
 
 const tx = contactDb.transaction('jate', 'readwrite');
 
@@ -27,10 +27,10 @@ const result = await request;
 console.log('🚀 - data saved to the database', result);
 
 };
-// TODO: Add logic for a method that gets all the content from the database
+// Method that gets all the content from the database
 export const getDb = async () => {console.error('getDb not implemented');
 
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   const tx = contactDb.transaction('jate', 'readonly');
 
@@ -41,5 +41,6 @@ export const getDb = async () => {console.error('getDb not implemented');
   const result = await request;
   console.log('result.value', result);
   return result;
-}
+};
+
 initdb();
