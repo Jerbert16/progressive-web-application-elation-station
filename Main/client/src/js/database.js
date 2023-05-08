5452e32e3f05fe85383e99618f57fd5ac0d1c94c
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-05-08 13:54:21 -0600

    made naming tweaks

--- Main/client/src/js/database.js
+++ Main/client/src/js/database.js
@@ -15,8 +15,6 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {console.error('putDb not implemented');
 
-console.log('Post to the database');
-
 const contactDb = await openDB('jate', 1);
 
 const tx = contactDb.transaction('jate', 'readwrite');
@@ -31,7 +29,6 @@ console.log('🚀 - data saved to the database', result);
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {console.error('getDb not implemented');
-console.log('GET from the database');
 
   const contactDb = await openDB('jate', 1);
 
